79783f775ccc3cee18906e947564e241b668a9b0
Caleb Oki <caleboki@gmail.com>
2020-02-23 16:41:25 +0100

    base url config

--- client-nuxt/nuxt.config.js
+++ client-nuxt/nuxt.config.js
@@ -47,7 +47,7 @@ export default {
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
-    baseURL: 'http://localhost:8000/api'
+    baseURL: process.env.BASE_URL || 'http://localhost:8000/api'
   },
   /*
   ** Build configuration
